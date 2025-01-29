a7057139088d9db41cf8cb46b5d38d140a481832
chris <cms8904@g.rit.edu>
2025-01-29 13:48:13 -0500

    removed random file

--- assets/scripts/rhythm/MetronomePlayer.cs
+++ assets/scripts/rhythm/MetronomePlayer.cs
@@ -6,18 +6,6 @@ public partial class MetronomePlayer : AudioStreamPlayer
     [Export] public AudioStreamMP3 tickSound;
     [Export] public AudioStreamMP3 accentedTickSound;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-
-    }
-
     public void PlayTick()
     {
         Stream = tickSound;
