05af560bf50598b0ddca5e54509c574ee57b13f0
chris <cms8904@g.rit.edu>
2025-02-28 13:28:14 -0500

    going to 4.4

--- assets/scripts/rhythm/InteractiveAudioTester.cs
+++ assets/scripts/rhythm/InteractiveAudioTester.cs
@@ -4,23 +4,19 @@ using System;
 public partial class InteractiveAudioTester : AudioStreamPlayer
 {
 
-	AudioStreamInteractive stream;
+	[Export] AudioStreamPlayer player;
+	[Export] public Label label;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		stream = Stream as AudioStreamInteractive;
-
-		Finished += () =>
-		{
-			GD.Print("yo its done");
-			Play();
-		};
+		//stream = GetNode<AudioStreamInteractive>("AudioStreamPla");
+		GD.Print("lets go");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		label.Text = player.Stream._GetStreamName();
 	}
 }
