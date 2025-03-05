807db6a99b0e67968a98cf0b15c2ed28988fbdcb
kaConn40 <kc4025@rit.edu>
2025-03-05 16:44:36 -0500

    github being annoying

--- assets/scripts/UI/PauseManager.cs
+++ assets/scripts/UI/PauseManager.cs
@@ -11,8 +11,14 @@ public partial class PauseManager : Node
     private bool isPaused = false;
     private bool playHit = false;
     private bool inOptions = false;
+<<<<<<< Updated upstream
     private Texture2D resumeNormal = GD.Load<Texture2D>("res://assets/UI/resume.png");
     private Texture2D resumeHover= GD.Load<Texture2D>("res://assets/UI/resume_Hover.png");
+=======
+    private Texture2D buttonNormal = GD.Load<Texture2D>("res://assets/UI/resume.png");
+    private Texture2D buttonHover = GD.Load<Texture2D>("res://assets/UI/resume_Hover.png");
+
+>>>>>>> Stashed changes
     // Called when the node enters the scene tree for the first time.
     public override void _EnterTree()
     {
@@ -25,6 +31,7 @@ public partial class PauseManager : Node
     }
     public override void _Input(InputEvent @event)
     {
+<<<<<<< Updated upstream
         if (@event is InputEventKey inputEventKey && inputEventKey.Pressed && !inOptions)
         {
             if (inputEventKey.Keycode != Key.Escape) return;
@@ -35,11 +42,21 @@ public partial class PauseManager : Node
                 GetTree().Paused = isPaused;
             }
            
+=======
+        if (@event is InputEventKey inputEventKey && inputEventKey.Pressed &&!inOptions)
+        {
+            if (inputEventKey.Keycode != Key.Escape) return;
+            isPaused = !isPaused;
+
+            EmitSignal(SignalName.GamePauseToggle, isPaused,playHit);
+            GetTree().Paused = isPaused;
+>>>>>>> Stashed changes
 
         }
     }
     public void OnResumePressed()
     {
+<<<<<<< Updated upstream
        
         if(!playHit)
         {
@@ -58,6 +75,30 @@ public partial class PauseManager : Node
         }
         EmitSignal(SignalName.GamePauseToggle, isPaused, playHit);
 
+=======
+        //will change the play button to a resume button and launch the game
+        if (!playHit)
+        {
+
+            TextureButton button = GetNode<TextureButton>("PauseMenu/resume");
+            button.TextureNormal = buttonNormal;
+            button.TextureHover= buttonHover;
+            PackedScene test = GD.Load<PackedScene>("res://scenes/main_game.tscn");
+            Node gameSceneNode = test.Instantiate();
+            GetParent().AddChild(gameSceneNode);
+            playHit = true;
+            EmitSignal(SignalName.GamePauseToggle, isPaused,playHit);
+        }
+        //will use this otherwise
+        else
+        {
+            isPaused = !isPaused;
+            EmitSignal(SignalName.GamePauseToggle, isPaused, playHit);
+            GetTree().Paused = isPaused;
+        }
+      
+        
+>>>>>>> Stashed changes
     }
     public void OnQuitPressed()
     {
