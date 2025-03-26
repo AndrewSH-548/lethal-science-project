7d198214de292db278a7595e435579a92d6e2abb
chris <cms8904@g.rit.edu>
2025-03-26 13:25:05 -0400

    esc will trigger menu visibility

--- assets/scripts/UI/PauseManager.cs
+++ assets/scripts/UI/PauseManager.cs
@@ -6,58 +6,65 @@ public partial class PauseManager : Node
     public static PauseManager Instance { get; private set; }
 
     [Signal]
-    public delegate void GamePauseToggleEventHandler(bool isPaused,bool playHit);
+    public delegate void GamePauseToggleEventHandler();
 
     private bool isPaused = false;
-    private bool playHit = false;
+    private bool gamePlayedForFirstTime = true;
     private bool inOptions = false;
     private Texture2D resumeNormal = GD.Load<Texture2D>("res://assets/UI/resume.png");
     private Texture2D resumeHover= GD.Load<Texture2D>("res://assets/UI/resume_Hover.png");
     // Called when the node enters the scene tree for the first time.
+
     public override void _EnterTree()
     {
         Instance = this;
-       
-
     }
+
     public override void _Input(InputEvent @event)
     {
         if (@event is InputEventKey inputEventKey && inputEventKey.Pressed && !inOptions)
         {
-            if (inputEventKey.Keycode != Key.Escape) return;
-            if(playHit)
+            if(inputEventKey.Keycode == Key.Escape)
             {
-                isPaused = !isPaused;
-                EmitSignal(SignalName.GamePauseToggle, isPaused, playHit);
-                GetTree().Paused = isPaused;
+                TogglePause();
             }
-           
-
         }
     }
 
-
-public void OnResumePressed()
+    /// <summary>
+    /// Opens/closes the pause screen and tells the SceneTree that the game is paused/unpaused.
+    /// </summary>
+    private void TogglePause()
     {
-       
-        if(!playHit)
+        isPaused = !isPaused;
+        EmitSignal(SignalName.GamePauseToggle);
+        GetTree().Paused = isPaused;
+    }
+
+    public void OnResumePressed()
+    {
+        if(gamePlayedForFirstTime)
         {
+            // change "Play" button to "Resume"
             TextureButton resumeButton = GetNode<TextureButton>("PauseMenu/resume");
             resumeButton.TextureNormal = resumeNormal;
             resumeButton.TextureHover = resumeHover;
+
+            // instantiate the game scene
             PackedScene gameScene = GD.Load<PackedScene>("res://scenes/main_game.tscn");
             Node gameNode = gameScene.Instantiate();
             GetParent().AddChild(gameNode);
-            playHit = true;  
+
+            GetNode<CanvasLayer>("PauseMenu").Hide();
+
+            gamePlayedForFirstTime = false;
         }
         else
         {
-            isPaused = !isPaused;
-            GetTree().Paused = isPaused;
+            TogglePause();
         }
-        EmitSignal(SignalName.GamePauseToggle, isPaused, playHit);
-
     }
+
     public void OnQuitPressed()
     {
         GetTree().Quit();
@@ -76,5 +83,4 @@ public void OnResumePressed()
         inOptions = false;
         options.Hide();
     }
-  
 }
--- assets/scripts/UI/TogglePauseVisibility.cs
+++ assets/scripts/UI/TogglePauseVisibility.cs
@@ -3,28 +3,14 @@ using System;
 
 public partial class TogglePauseVisibility : CanvasLayer
 {
-    [Export]public bool visibleOnPause = true;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		PauseManager.Instance.GamePauseToggle += ToggleVisibility;
 	}
-	public void ToggleVisibility(bool isPaused,bool playHit)
+
+	public void ToggleVisibility()
 	{
-        if (!playHit)
-		{
-			Show();
-			return;
-		}
-
-		if(visibleOnPause==isPaused)
-		{
-			Show();
-		}
-		else
-		{
-			Hide();
-		}
+        Visible = !Visible;
 	}
-
 }
