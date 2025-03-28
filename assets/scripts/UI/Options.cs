ef1281f170293487af2d4689d10cabe076b55b3f
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-03-28 11:33:42 -0400

    volume slider range adjusted

--- assets/scripts/UI/Options.cs
+++ assets/scripts/UI/Options.cs
@@ -6,8 +6,8 @@ public partial class Options : CanvasLayer
     void OnVolumeSliderChanged(float value)
     {
         int masterBusIndex = AudioServer.GetBusIndex("Master");
-        if (value <= -80) AudioServer.SetBusMute(masterBusIndex, true);
-        if (value > -80 && AudioServer.IsBusMute(masterBusIndex)) AudioServer.SetBusMute(masterBusIndex, false);
+        if (value <= -30) AudioServer.SetBusMute(masterBusIndex, true);
+        if (value > -30 && AudioServer.IsBusMute(masterBusIndex)) AudioServer.SetBusMute(masterBusIndex, false);
         AudioServer.SetBusVolumeDb(masterBusIndex, value);
         GD.Print(AudioServer.GetBusVolumeDb(masterBusIndex));
     }
